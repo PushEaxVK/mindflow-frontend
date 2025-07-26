af074fbd0e90ae06f0e8692005a71e1e43056c68
amurovnaa <amurovna09@gmail.com>
2025-07-26 10:08:48 +0400

    add private and restricted routes + all pages (App.jsx)

---
+++ src/pages/AuthorProfilePage/AuthorProfilePage.jsx
@@ -0,0 +1,13 @@
+import Container from '../../components/Container/Container';
+
+const AuthorProfilePage = () => {
+  return (
+    <section>
+      <Container>
+        <h1>AuthorProfilePage!</h1>
+      </Container>
+    </section>
+  );
+};
+
+export default AuthorProfilePage;
