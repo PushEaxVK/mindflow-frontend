168edb0cb03755afa13b1ac501a993d8daea2840
Andrii Semenenko <oxonomy14@gmail.com>
2025-07-27 06:56:01 +0700

    v0520-260725 (#3)
    * v0520-260725
    
    * v1540-260725
    
    * v1540-260725
    
    * v1747-260725
    
    * v1825-260725
    
    * v1830-260725
    
    * v1855-260725
    
    * v2254-260725
    
    * v2327-260725

---
+++ src/components/nestedRoutes/MyArticles/MyArticles.jsx
@@ -0,0 +1,10 @@
+import ArticlesList from '../../ArticlesList/ArticlesList.jsx';
+const MyArticles = () => {
+  return (
+    <>
+      <ArticlesList icon={'icon-edit-article'} btnStyle={'EditArticle'} />
+    </>
+  );
+};
+
+export default MyArticles;
