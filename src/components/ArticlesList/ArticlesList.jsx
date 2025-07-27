d3b2bb79d3006c957f8267ee8f513ce917c36e98
Andrii Semenenko <oxonomy14@gmail.com>
2025-07-27 19:42:13 +0700

    v1942-270725

--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -1,5 +1,6 @@
 import css from './ArticlesList.module.css';
-import { Link } from 'react-router-dom';
+
+import ArticlesItem from '../ArticlesItem/ArticlesItem.jsx';
 
 const ArticlesList = ({
   icon = 'icon-edit-article',
@@ -8,36 +9,7 @@ const ArticlesList = ({
   return (
     <>
       <ul className={css.articlesList}>
-        <li className={css.articlesItem}>
-          <img
-            className={css.imgArticle}
-            src="https://res.cloudinary.com/dfoiy9rn5/image/upload/v1753462124/article_img-2_whyhpt.png"
-            alt=""
-          />
-          <div className={css.infoArticle}>
-            <p className={css.authorArticle}>Clark</p>
-            <p className={css.title}>
-              When Anxiety Feels Like a Room With No Doors
-            </p>
-            <p className={css.desc}>
-              10 advices how mediations can help you feeling better
-            </p>
-          </div>
-          <div className={css.navButton}>
-            <Link to="/" className={css.btnLearnMore} aria-label="Learn more">
-              Learn more
-            </Link>
-            <button aria-label="edit Article" className={css[`btn${btnStyle}`]}>
-              <svg
-                className={css[`svgIcon${btnStyle}`]}
-                width="14.25"
-                height="14.25"
-              >
-                <use href={`/public/icons-profileArticles.svg#${icon}`}></use>
-              </svg>
-            </button>
-          </div>
-        </li>
+        <ArticlesItem icon={icon} btnStyle={btnStyle} />
       </ul>
     </>
   );
