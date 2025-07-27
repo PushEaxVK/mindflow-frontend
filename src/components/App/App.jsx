7885b19ab6b0cba5149dde8c6c3c9e4ee9ff6988
amurovnaa <151229291+amurovnaa@users.noreply.github.com>
2025-07-27 04:04:33 +0400

    add private and restricted routes + all pages (App.jsx) (#6)
    Co-authored-by: PushEaxVK <98906196+PushEaxVK@users.noreply.github.com>

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,10 +6,33 @@ import { Route, Routes } from 'react-router-dom';
 import { RestrictedRoute } from '../RestrictedRoute';
 import { PrivateRoute } from '../PrivateRoute';
 import Layout from '../Layout/Layout';
+import Loader from '../../components/Loader/Loader.jsx';
 
 const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const ArticlePage = lazy(() =>
+  import('../../pages/ArticlePage/ArticlePage.jsx')
+);
+const ArticlesPage = lazy(() =>
+  import('../../pages/ArticlesPage/ArticlesPage.jsx')
+);
+const AuthorProfilePage = lazy(() =>
+  import('../../pages/AuthorProfilePage/AuthorProfilePage.jsx')
+);
+const AuthorsPage = lazy(() =>
+  import('../../pages/AuthorsPage/AuthorsPage.jsx')
+);
+const CreateArticlePage = lazy(() =>
+  import('../../pages/CreateArticlePage/CreateArticlePage.jsx')
+);
+const LoginPage = lazy(() => import('../../pages/LoginPage/LoginPage.jsx'));
+const RegisterPage = lazy(() =>
+  import('../../pages/RegisterPage/RegisterPage.jsx')
+);
+const UploadPhoto = lazy(() =>
+  import('../../pages/UploadPhoto/UploadPhoto.jsx')
+);
 
-function App() {
+const App = () => {
   const dispatch = useDispatch();
   const isRefreshing = useSelector(selectIsRefreshing);
 
@@ -17,15 +40,54 @@ function App() {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return isRefreshing ? null : (
-    <Suspense fallback={null}>
+  if (isRefreshing) {
+    return <Loader />;
+  }
+
+  return (
+    <Suspense fallback={<Loader />}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
+          <Route path="articles" element={<ArticlesPage />} />
+          <Route path="articles/:id" element={<ArticlePage />} />
+          <Route path="authors" element={<AuthorsPage />} />
+          <Route path="authors/:id" element={<AuthorProfilePage />} />
+
+          <Route
+            path="create"
+            element={
+              <PrivateRoute
+                redirectTo="/login"
+                component={<CreateArticlePage />}
+              />
+            }
+          />
+          <Route
+            path="photo"
+            element={
+              <PrivateRoute redirectTo="/login" component={<UploadPhoto />} />
+            }
+          />
+
+          <Route
+            path="login"
+            element={
+              <RestrictedRoute redirectTo="/" component={<LoginPage />} />
+            }
+          />
+          <Route
+            path="register"
+            element={
+              <RestrictedRoute redirectTo="/" component={<RegisterPage />} />
+            }
+          />
+
+          <Route path="*" element={<HomePage />} />
         </Route>
       </Routes>
     </Suspense>
   );
-}
+};
 
 export default App;
