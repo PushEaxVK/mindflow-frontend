2d12b16118b3a3ea86145504aeee302c4be70526
myroslava <murosja80@gmail.com>
2025-07-28 23:30:26 +0300

    popularArticles

--- src/components/TopCreators/TopCreators.jsx
+++ src/components/TopCreators/TopCreators.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './TopCreators.module.css';
-import CreatorCard from '../CreatorCard/CreatorCard.jsx';
+import CreatorCard from '../CreatorCard/CreatorCard';
 
 function TopCreators() {
   const [creators, setCreators] = useState([]);
