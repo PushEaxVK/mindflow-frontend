e1165e0f8d3ed789f34275c9907d9c971a683bf2
Andrii Semenenko <oxonomy14@gmail.com>
2025-07-29 07:20:37 +0700

    articlesPage-v2034-2807 (#13)

--- src/pages/ArticlesPage/ArticlesPage.jsx
+++ src/pages/ArticlesPage/ArticlesPage.jsx
@@ -1,10 +1,27 @@
 import Container from '../../components/Container/Container';
+import css from './ArticlesPage.module.css';
+import ArticlesDropdown from '../../components/ArticlesDropdown/ArticlesDropdown';
+import ArticlesList from '../../components/ArticlesList/ArticlesList';
+import LoadMore from '../../components/LoadMore/LoadMore';
 
 const ArticlesPage = () => {
   return (
     <section>
       <Container>
-        <h1>ArticlesPage!</h1>
+        <h1 className={css.profileTitle}>Articles</h1>
+        <div className={css.boxSelect}>
+          <div>
+            <p className={css.countArticles}>96 articles</p>
+          </div>
+          <div className={css.formSelect}>
+            <ArticlesDropdown />
+          </div>
+        </div>
+        <ArticlesList
+          icon={'icon-favorite-article'}
+          btnStyle={'FavoriteArticleNotSaved'}
+        />
+        <LoadMore />
       </Container>
     </section>
   );
