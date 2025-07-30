d92ea3b7ae88847dedb3970b66eb49721e565851
myroslava <murosja80@gmail.com>
2025-07-30 15:13:32 +0300

    reg

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,16 @@
-import Container from '../../components/Container/Container';
 import Hero from '../../components/Hero/Hero.jsx';
 import AboutUs from '../../components/AboutUs/AboutUs.jsx';
 import PopularArticles from '../../components/PopularArticles/PopularArticles.jsx';
 import TopCreators from '../../components/TopCreators/TopCreators.jsx';
-import styles from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <section className={styles.HomePage}>
-      <Container>
-        <Hero />
-        <AboutUs />
-        <PopularArticles />
-        <TopCreators />
-      </Container>
-    </section>
+    <>
+      <Hero />
+      <AboutUs />
+      <PopularArticles />
+      <TopCreators />
+    </>
   );
 };
 
