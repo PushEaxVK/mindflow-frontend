419815c09a21cc39bedee891fca6c9088f270314
MykhayloGor <ursa.mykh@gmail.com>
2025-07-30 20:07:01 +0200

    refresh req logic fixed

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+// axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://mindflow-backend-iwk7.onrender.com';
+
 axios.defaults.withCredentials = true;
 
 export const setAuthHeader = (token) => {
@@ -27,7 +29,7 @@ export const signup = async ({ name, email, password }) => {
     email,
     password,
   });
-  
+
   const transformedData = transformAuthResponse(response.data.data);
   setAuthHeader(transformedData.token);
   return { data: transformedData };
@@ -49,7 +51,7 @@ export const logout = async () => {
 export const refresh = async () => {
   const response = await axios.post('/auth/refresh');
   const responseData = response.data?.data || response.data;
-  
+
   if (responseData && responseData.accessToken) {
     const transformedData = transformAuthResponse(responseData);
     setAuthHeader(transformedData.token);
