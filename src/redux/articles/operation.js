326ace2b0ff53870ae735e44b183c8fca63eb35e
Andrii Semenenko <oxonomy14@gmail.com>
2025-07-31 03:49:38 +0700

    add LoadMore for ArticlesPage-v0349-310725

--- src/redux/articles/operation.js
+++ src/redux/articles/operation.js
@@ -1,35 +1,9 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// export const fetchAllArticles = createAsyncThunk(
-//   'articles/fetchAll',
-//   async ({ page = 1, filter = 'all' }, thunkAPI) => {
-//     try {
-//       const baseUrl = filter === 'popular' ? '/articles/popular' : '/articles';
-//       const url = `${baseUrl}?limit=12&page=${page}`;
-
-//       const response = await axios.get(url);
-
-//       const data = Array.isArray(response.data)
-//         ? {
-//             articles: response.data,
-//             total: response.data.length,
-//             page: 1,
-//             pages: 1,
-//           }
-//         : response.data;
-
-//       return data;
-//     } catch (error) {
-//       console.log(error);
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
 export const fetchAllArticles = createAsyncThunk(
   'articles/fetchAll',
-  async ({ page = 1, limit = 3, filter = '' }, thunkAPI) => {
+  async ({ page = 1, limit = 12, filter = '' }, thunkAPI) => {
     try {
       let url = '';
 
