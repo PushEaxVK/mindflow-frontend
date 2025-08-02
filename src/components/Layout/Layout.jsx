77467d669b502d5d426c04360e59847e8804cfe7
PushEaxVK <98906196+PushEaxVK@users.noreply.github.com>
2025-08-03 00:08:56 +0300

    fix footer (#51)

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,13 @@
 import AppBar from '../AppBar/AppBar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Footer/Footer';
-import ModalMenu from '../ModalMenu/ModalMenu.jsx';
+import ModalMenu from '../ModalMenu/ModalMenu';
 import { useSelector } from 'react-redux';
 import {
   selectModalIsOpen,
   selectModalType,
 } from '../../redux/modal/selectors.js';
 import Modal from '../Modal/Modal.jsx';
-import Footer from '../Footer/Footer';
 
 const Layout = () => {
   const isOpen = useSelector(selectModalIsOpen);
@@ -25,4 +24,5 @@ const Layout = () => {
     </>
   );
 };
+
 export default Layout;
