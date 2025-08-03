fd915dfe2ec6c6a8d42eb4eabc111e317c168241
Molnia00 <159462069+Molnia00@users.noreply.github.com>
2025-08-03 21:10:25 +0200

    testing uploadPhoto component (#54)
    * adding uploadPhoto component
    
    * Update UploadPhoto.jsx
    
    ---------
    
    Co-authored-by: PushEaxVK <98906196+PushEaxVK@users.noreply.github.com>

---
+++ src/pages/UploadPhoto/UploadPhoto.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { uploadPhoto } from '../../redux/uploadPhoto/action';
+
+
+const UploadPhoto = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const dispatch = useDispatch();
+  const { loading, photoUrl, error } = useSelector(state => state.photo);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    if (!selectedFile) {
+      alert('Будь ласка, виберіть файл для завантаження!');
+      return;
+    }
+    dispatch(uploadPhoto(selectedFile));
+  };
+
+  return (
+    <div>
+      <h2>Upload your photo</h2>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload} disabled={loading}>
+        {loading ? 'Завантаження...' : 'Save'}
+      </button>
+
+      {error && <p>{error}</p>}
+      
+      {photoUrl && (
+        <div>
+          <p>Фото успішно завантажено!</p>
+          <img src={photoUrl} alt="Завантажене фото" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UploadPhoto;
