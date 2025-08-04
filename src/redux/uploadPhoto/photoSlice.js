e7cf9c5c8763490748469ae86e803479671d9baa
Molnia00 <bilankati@gmail.com>
2025-08-04 21:14:41 +0200

    Update photoSlice.js

--- src/redux/uploadPhoto/photoSlice.js
+++ src/redux/uploadPhoto/photoSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 
-// Створюємо асинхронний thunk для запиту до API
+
 export const uploadPhoto = createAsyncThunk(
   'photo/uploadPhoto',
   async (file, thunkAPI) => {
@@ -15,22 +15,21 @@ export const uploadPhoto = createAsyncThunk(
       });
 
       if (!response.ok) {
-        // Якщо відповідь не успішна, створюємо об'єкт помилки
+        
         const errorData = await response.json();
         throw new Error(errorData.message || 'Помилка при завантаженні фото.');
       }
 
       const result = await response.json();
-      toast.success('Фото успішно завантажено!'); // Виводимо сповіщення про успіх
+      toast.success('Фото успішно завантажено!'); 
       return result.data.photo;
     } catch (error) {
-      toast.error(error.message); // Виводимо сповіщення про помилку
+      toast.error(error.message); 
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
-// Створюємо slice
 const photoSlice = createSlice({
   name: 'photo',
   initialState: {
@@ -38,20 +37,20 @@ const photoSlice = createSlice({
     loading: false,
     error: null,
   },
-  reducers: {}, // Тут можна додати синхронні редюсери, якщо вони потрібні
+  reducers: {},
   extraReducers: (builder) => {
     builder
-      // Обробка стану "pending"
+      
       .addCase(uploadPhoto.pending, (state) => {
         state.loading = true;
         state.error = null;
       })
-      // Обробка стану "fulfilled" (успішно)
+      
       .addCase(uploadPhoto.fulfilled, (state, action) => {
         state.loading = false;
         state.photoUrl = action.payload;
       })
-      // Обробка стану "rejected" (помилка)
+      
       .addCase(uploadPhoto.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
