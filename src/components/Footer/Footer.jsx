a871d4c2dc86792be722dde76a1133c2d9b33cd3
polly <polifibliss@gmail.com>
2025-08-04 20:57:26 +0300

    edit footer.jsx (#65)
    * add footer
    
    * edit footer
    
    * edit footer logo and styles
    
    * edit footer.jsx
    
    * edit footer.jsx
    
    * edit footer.jsx
    
    * edit footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,29 @@
 import { Link } from 'react-router-dom';
 import Container from '../Container/Container';
 import css from './Footer.module.css'; // Import the CSS module for this component
+import { selectUser } from "../../redux/auth/selectors.js";
+import { useSelector } from 'react-redux';
 
 const Footer = () => {
+  const user = useSelector(selectUser);
   return (
+    <footer className={css.footer}>
     <Container noVerticalPadding>
-      <footer className={css.footer}>
-        <div className={css.inner}>
-          <div className={css.logo}>harmoniq</div>
-          <p className={css.copyright}>© 2025 Harmoniq. All rights reserved.</p>
-          <nav className={css.links}>
-            <ul className={css.linkList}>
-              <li>
-                <Link to="/articles" className={css.link}>
-                  Articles
-                </Link>
-              </li>
-              <li>
-                <Link to="/account" className={css.link}>
-                  Account
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
+      <div className={css.inner}>
+        <svg className={css.logo}>
+              <use href="/img/icons.svg#icon-logo-min"></use>
+            </svg>
+        <p className={css.copyright}>© 2025 Harmoniq. All rights reserved.</p>
+        <nav className={css.links}>
+          <ul className={css.linkList}>
+            <li><Link to="/articles" className={css.link}>Articles</Link></li>
+            <li><Link to="/authors/${user.id}" className={css.link}>Account</Link></li>
+          </ul>
+        </nav>
+      </div>
+      </Container>
       </footer>
-    </Container>
   );
 };
 
-export default Footer;
+export default Footer; 
