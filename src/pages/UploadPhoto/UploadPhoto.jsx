e374b59a7d0fe35bf99c7622f288a3582a183e14
Molnia00 <bilankati@gmail.com>
2025-08-04 21:55:51 +0200

    error

--- src/pages/UploadPhoto/UploadPhoto.jsx
+++ src/pages/UploadPhoto/UploadPhoto.jsx
@@ -1,44 +1,18 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { uploadPhoto } from '../../redux/uploadPhoto/action';
 
 
-const UploadPhoto = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
 
-  const dispatch = useDispatch();
-  const { loading, photoUrl, error } = useSelector(state => state.photo);
+function UploadPhotoUser() {
+    
 
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
 
-  const handleUpload = () => {
-    if (!selectedFile) {
-      alert('Будь ласка, виберіть файл для завантаження!');
-      return;
-    }
-    dispatch(uploadPhoto(selectedFile));
-  };
 
-  return (
-    <div>
-      <h2>Upload your photo</h2>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload} disabled={loading}>
-        {loading ? 'Завантаження...' : 'Save'}
-      </button>
-
-      {error && <p>{error}</p>}
-      
-      {photoUrl && (
-        <div>
-          <p>Фото успішно завантажено!</p>
-          <img src={photoUrl} alt="Завантажене фото" />
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default UploadPhoto;
+    return (
+        <>
+            <div>
+                <h2>Upload your photo</h2>
+                <input type="file" />
+                
+            </div>
+        </>
+    )
+}
