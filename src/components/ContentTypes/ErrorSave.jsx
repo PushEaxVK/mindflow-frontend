15f2315243156eab6aad7b8c7c20fa8d9e1224ce
amurovnaa <amurovna09@gmail.com>
2025-08-05 15:41:17 +0400

    fix errorSave modal text

--- src/components/ContentTypes/ErrorSave.jsx
+++ src/components/ContentTypes/ErrorSave.jsx
@@ -22,7 +22,7 @@ const ErrorSave = () => {
     <div className={s.modal__wrapper}>
       <h2 className={s.modal__headline}>Error while saving</h2>
       <p className={s.modal__text}>
-        To save this article, you need to be authorize first
+        To save this article, you need to authorize first
       </p>
       <div className={s.modal__btns}>
         <button className={clsx(s.modal__btn, s.second)} onClick={handleLogin}>
